07aac0278cffa299f148009d9fa1b4b7c4334002
ald19 <ald19@alu.ua.es>
2021-06-29 18:42:25 +0200

    Tests para login, registro y listado de recetas finalizados.

---
+++ backend/src/tests/auth.test.js
@@ -0,0 +1,109 @@
+const request = require('supertest');
+const app = require('../index');
+
+/**
+ * Tests para comprobar el correcto funcionamiento del login
+ */
+describe('POST /api/login', () => {
+    it('Se devuelve un mensaje "Login completado" si el login se ha realizado correctamente', done => {
+        const data = {
+            email: 'alex@ua.es',
+            password: '2345'
+        }
+        request(app)
+            .post('/api/login')
+            .send(data)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /text/)
+            .expect(200)
+            .expect('Login completado')
+            .end(err => {
+                if(err) return done(err);
+                done();
+            })
+    });
+
+    it('Se devuelve un mensaje "Credenciales inválidas" si el login no se ha realizado correctamente', done => {
+        const data = {
+            email: 'alex@ua.es',
+            password: '0000'
+        }
+        request(app)
+            .post('/api/login')
+            .send(data)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /text/)
+            .expect(400)
+            .expect('Credenciales inválidas')
+            .end(err => {
+                if(err) return done(err);
+                done();
+            })
+    });
+});
+
+/**
+ * Tests para comprobar el correcto funcionamiento del registro
+ */
+ describe('POST /api/registro', () => {
+    it('Se devuelve un mensaje "El usuario ha sido registrado correctamente" indicando que el registro se ha realizado correctamente', done => {
+        const data = {
+            name: 'test',
+            nickname: 'test',
+            email: 'test@ua.es',
+            password: '12345',
+            birth_date: '1980-01-01'
+        }
+        request(app)
+            .post('/api/registro')
+            .send(data)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /text/)
+            .expect(200)
+            .expect('El usuario ha sido registrado correctamente')
+            .end(err => {
+                if(err) return done(err);
+                done();
+            })
+    });
+
+    it('Se devuelve un mensaje de error "Ya hay un usuario registrado con ese email o faltan campos" si ya existe un usuario con ese email', done => {
+        const data = {
+            name: 'test',
+            nickname: 'test',
+            email: 'alex@ua.es',
+            password: '12345',
+            birth_date: '1980-01-01'
+        }
+        request(app)
+            .post('/api/registro')
+            .send(data)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /text/)
+            .expect(400)
+            .expect('Ya hay un usuario registrado con ese email o faltan campos')
+            .end(err => {
+                if(err) return done(err);
+                done();
+            })
+    });
+
+    it('Se devuelve un mensaje de error "Ya hay un usuario registrado con ese email o faltan campos" si falta algún campo por rellenar', done => {
+        const data = {
+            name: 'test',
+            email: 'test2@ua.es',
+            password: '12345',
+        }
+        request(app)
+            .post('/api/registro')
+            .send(data)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /text/)
+            .expect(400)
+            .expect('Ya hay un usuario registrado con ese email o faltan campos')
+            .end(err => {
+                if(err) return done(err);
+                done();
+            })
+    })
+ });
---
+++ backend/src/tests/recipes.test.js
@@ -0,0 +1,49 @@
+const request = require('supertest');
+const app = require('../index');
+
+/**
+ * Test que comprueba que el listado de recetas se devuelve correctamente
+ */
+describe('GET /api/recipes', () => {
+    it('Se devuelve un JSON que contiene el listado de todas las recetas', done => {
+        request(app)
+            .get('/api/recetas')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
+    });
+});
+
+/**
+ * Test que comprueba si una receta en concreto se devuelve correctamente
+ */
+describe('GET /api/recipes/:id', () => {
+    it('Se devuelve un JSON que contiene la información de una receta en específico', done => {
+        request(app)
+            .get('/api/recetas/1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect({
+                id: 1,
+                nombre: "Pollo con salsa",
+                descripcion: "Un pollo con una salsa especiada",
+                duracion: "30min",
+                valoracion: 0,
+                id_usuario: 1
+            })
+            .expect(200, done);
+    });
+
+    it('Se devuelve un mensaje "No se han encontrado resultados" si la receta no existe', done => {
+        request(app)
+            .get('/api/recetas/0')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /text/)
+            .expect(404)
+            .expect('No se han encontrado resultados')
+            .end((err) => {
+                if(err) return done(err);
+                done();
+            });
+    });
+});
